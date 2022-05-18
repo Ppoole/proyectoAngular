c4877773b2463e648f25b4c4fbf62bc7fc5fc379
Pedro <ppoole@iespuntadelverde.es>
2022-05-18 09:26:23 +0200

    NuevaPersona funciona, no vincula a tel.

--- src/app/datos-persona/datos-persona.component.ts
+++ src/app/datos-persona/datos-persona.component.ts
@@ -11,20 +11,21 @@ import { ObtenPersonaService } from '../shared/servicios/ObtenPersona.service';
 })
 export class DatosPersonaComponent implements OnInit {
   @Input() numeroEnBusqueda:string;
-
-  
   constructor(private ObtenPersonaService:ObtenPersonaService, private NuevaPersonaService:NuevaPersonaService) { }
-  personaEnMemoria:Persona=undefined;
+
+  personaEnMemoria:Persona=undefined;//Creo la variable persona en memoria de donde beberé muchos valores para el HTML
+
+  /**Al iniciarse, coge el número que hay introducido y lo busca en la base de datos. Si está, lo carga, y si no está, crea una persona que introduce inmediatamente en la BDD */
   ngOnInit(): void {
     
     let numeroABuscar=this.numeroEnBusqueda;
     
     this.ObtenPersonaService.obtenPersona(numeroABuscar).subscribe(datosPersona => {
-      if (datosPersona!=undefined&&datosPersona.length!=0){
-      let personaActual=new Persona(datosPersona[0]);
-      this.personaEnMemoria=personaActual;
+      if (datosPersona!=undefined&&datosPersona.length!=0){//Por si falla o retorna un array vacio
+      this.personaEnMemoria =new Persona(datosPersona[0]);
+      
       }
-      else{
+      else{//Aquí es donde crea una persona de humor neutro si no encuentra a nadie.
         
         this.personaEnMemoria=new Persona({nombre:'',detalles:'',nivelContento:3});
 
@@ -37,6 +38,9 @@ export class DatosPersonaComponent implements OnInit {
       })
   }
 
+  /**Genero esta función para poder actualizar los datos cada vez que se cambia el número, si no, sólo tendríamos los primeros onInit, que de hecho no es ninguno pues el onInit
+   * entra antes que el numero. Esta función es llamada desde app.component.ts
+   */
   actualizar(tel:string){
     let numeroABuscar=tel;
     
@@ -54,7 +58,7 @@ export class DatosPersonaComponent implements OnInit {
 
         });
 
-        
+
       }
       })
   }
